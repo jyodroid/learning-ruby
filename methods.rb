c13bbc9ab321ede962048aa5adfde0cb571ed550
John Tangarife <jyodroid@gmail.com>
2020-03-30 05:08:01 -0500

    Methods tests

--- methods.rb
+++ methods.rb
@@ -8,6 +8,7 @@ require './document.rb'
 
 THE_ORIGINAL_CONSTANT = 'original'
 PI = '3.14159'
+@fonts = ['courier', 'times roman', 'helvetica']
 
 # Freeze mutable objects assigned to constants??
 
@@ -61,4 +62,90 @@ end
 
 def getting_pi_again(pi_string)
   pi_string.to_f
+end
+
+def print_fonts
+  for font in @fonts do
+    puts font
+  end
+end
+
+def print_fonts_well_done
+  @fonts.each do |font|
+    puts font
+  end
+end
+
+# This method has error intentionally since font sice will end on an in supperior index
+def another_print_fonts
+  for index in 0..@fonts.size do
+    puts "#{index} -> #{@fonts[index]}" # use of templating
+  end
+end
+
+def whos_books(title)
+  case title
+  when 'War And Peace'
+    puts 'Tolstoy'
+  when 'Romeo And Juliet'
+    puts 'Shakespeare'
+  else
+    puts "Don't know"
+  end
+end
+
+# The "compact" guy
+def get_author(title)
+  author =  case title
+            when 'War And Peace' then 'Tolstoy' # We acn use regexp /War And .*/
+            when 'Romeo And Juliet' then 'Shakespeare'
+            else "Don't know"
+            end
+  puts author
+end
+
+# A simple way to validate a non defined variable
+def method_empty_first_name
+  @first_name ||= '' #Equivalent to @first_name = '' unless @first_name and @first_name || ''
+  @first_name
+end
+
+def initialized_array
+  poem_words = %w{ twinkle little star how I wonder }
+  poem_words.each do |word|
+    puts word
+  end
+end
+
+# Ways to write a hash
+def same_hash
+  book_info = { :first_name => 'Russ', :last_name => 'Olsen' }
+  another_book_info = { first_name: 'Russ', last_name: 'Olsen' }
+  book_info == another_book_info
+end
+
+def iterate_hash
+  movie = { title: '2001', genre: 'sci fi', rating: 10 }
+  movie.each { |entry| pp entry }
+  # or
+  movie.each { |name, value| puts "#{name} => #{value}"} #this √
+end
+
+# pass multiple args, use echo_at_least_two("a", *%w{"yo soy grut"},"b") or 
+# echo_at_least_two("a", *%w{"yo soy grut"}, _) in order to ommit any of the args
+def echo_at_least_two(first_arg, *middle_args, last_arg)
+  puts "The first argument is #{first_arg}"
+  middle_args.each { |arg| puts "A middle argument is #{arg}" }
+  puts "The last argument is #{last_arg}"
+end
+
+def joint_fonts
+  puts @fonts.join(' ')
+  puts @fonts.join('√')
+  puts @fonts.join
+end
+
+# use find_idex_of_font 'helvetica' or find_idex_of_font('helvetica')
+def find_idex_of_font(searched_font)
+  @fonts.find_index { |font| font == searched_font }
 end
